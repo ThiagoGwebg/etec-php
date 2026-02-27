ff872ce5e1639a1ad724562394ab53a1be3e5362
ThiagoGwebg <thiagoribeiro18181@gmail.com>
2026-02-26 21:13:13 -0300

    Fix:tabuada

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ $j = 0;
 while($i < 10){
     while($j < 10){
         $resultado = $i * $j;
-        echo $i."x".¨$j."=".$resultado."<br>";
+        echo $i ."x".$j."=".$resultado."<br>";
         $j += 1;
     }
 
     $j += 1;
+
+    echo"--------<br>";
+    $i += 1;
 }
